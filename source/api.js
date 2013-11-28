4dc0c72b7ae21d02d7cbcc7b8958a2a1b1371683
George Czabania <george@czabania.com>
2013-11-28 20:51:17 +1300

    Add api wrapper

---
+++ source/api.js
@@ -0,0 +1,64 @@
+(function () {
+
+  'use strict';
+
+  var Api;
+
+  var Items, Mouse, Select, Drag, Drop;
+
+  Items = require('./items');
+  Mouse = require('./mouse');
+  Select = require('./select');
+  Drag = require('./drag');
+  Drop = require('./drop');
+
+  Api = function (options) {
+
+    this.drops = [];
+    this.parent = options.parent;
+
+    this.items = new Items({
+      parent: this.parent,
+      query: options.query
+    });
+
+    this.mouse = new Mouse({
+      parent: this.parent,
+      items: this.items
+    });
+
+    this.select = new Select({
+      mouse: this.mouse,
+      items: this.items
+    });
+
+    this.drag = new Drag({
+      mouse: this.mouse
+    });
+
+
+  };
+
+  Api.prototype.init = function () {
+    var i;
+    this.mouse.init();
+    for (i = 0; i < this.drops.length; i++) {
+      this.drops[i].init();
+    }
+  };
+
+  Api.prototype.drop = function (el) {
+    var drop = new Drop({
+      mouse: this.mouse,
+      el: el
+    });
+    this.drops.push(drop);
+  };
+
+  if (typeof window !== 'undefined') {
+    window.Mouse = Api;
+  }
+
+  module.exports = Api;
+
+}());
